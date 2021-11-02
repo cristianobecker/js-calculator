5018a0de65e9cb4abba839a789659e396f1d6883
Mia Menegali <eduardamenegali@outlook.com>
2021-11-02 06:45:23 -0300

    cleared js to start over

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-  
-const add = function() {
-	
-};
+function type() {
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
-module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
-};
+}
