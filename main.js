6b864500f32e4f77962ba091842479c54cd2c4c5
Mia Menegali <eduardamenegali@outlook.com>
2021-11-03 07:51:52 -0300

    corrected power command

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ function append(operand) {
     let number = document.getElementById('result').innerHTML;
     document.getElementById('result').innerHTML = number + operand;
 }
+
+function deleteAll() {
+    document.getElementById('result').innerHTML = "";
+}
+
+function finalResult() {
+    let result = document.getElementById('result').innerHTML;
+    if(result){
+        document.getElementById('result').innerHTML = eval(result);
+    }
+    else {
+        document.getElementById('result').innerHTML = "Input the desired problem for a solution."
+    }
+}
