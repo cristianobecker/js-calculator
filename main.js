7cfa5c67b41ee0a9ba3655fa41e5a66a5f958575
Mia Menegali <eduardamenegali@outlook.com>
2021-11-03 08:04:17 -0300

    added an important comment at the end of the code

--- main.js
+++ main.js
@@ -16,3 +16,7 @@ function finalResult() {
         document.getElementById('result').innerHTML = "Input the desired problem for a solution."
     }
 }
+
+/* note to come back to this later and replace eval with a more complex 'function' because of its security issues. as this code was rewritten more than one time, it is crucial to 'polish' it and fix any ocurring issues due to expressions that can lead to misevents.
+that said,
+this is NOT a finished code. please be mindful of that when reading it. as a work in progress, there will be future changes for a better final product. thank you very much. *\
